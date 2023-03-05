40b9861b29294efd3e5ca3483bdc7a21b251b29e
TranDangKhoi <baits16@gmail.com>
2023-03-05 23:31:24 +0700

    :+1: Add security to swagger

---
+++ DotNetBootcamp_API/DotNetBootcamp_API/Controllers/AuthTestController.cs
@@ -0,0 +1,26 @@
+﻿using DotNetBootcamp_API.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetBootcamp_API.Controllers
+{
+    [Route("api/authtest")]
+    [ApiController]
+    public class AuthTestController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<string>> GetSomething()
+        {
+            return "You're authenticated";
+        }
+
+        [HttpGet("{id:int}")]
+        [Authorize(Roles = SD.Role_Admin)]
+        public async Task<ActionResult<string>> GetSomething(int someIntValue)
+        {
+            // Authorization => Authentication + Some access
+            return "You're authenticated with Role of Admin";
+        }
+    }
+}
--- DotNetBootcamp_API/DotNetBootcamp_API/Program.cs
+++ DotNetBootcamp_API/DotNetBootcamp_API/Program.cs
@@ -2,8 +2,12 @@ using Azure.Storage.Blobs;
 using DotNetBootcamp_API.Data;
 using DotNetBootcamp_API.Models;
 using DotNetBootcamp_API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,10 +28,57 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequireUppercase = false;
     options.Password.RequireNonAlphanumeric = false;
 });
+var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
+builder.Services.AddAuthentication(u =>
+{
+    u.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    u.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(u =>
+{
+    u.RequireHttpsMetadata = false;
+    u.SaveToken = true;
+    u.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddCors();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+    {
+        Description =
+        "JWT Authorization Header using the Bearer scheme \r\n\r\n" +
+        "Enter 'Bearer' [space] and then your token in the text input below. \r\n\r\n" +
+        "Example: \"Bearer 1234556\"",
+        Name = "Authorization",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Scheme = JwtBearerDefaults.AuthenticationScheme
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+            },
+        new List<string>()
+        },
+    });
+});
 
 var app = builder.Build();
 
@@ -39,7 +90,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(o => o.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
